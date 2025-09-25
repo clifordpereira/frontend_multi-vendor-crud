5aca6b10dfa1106f657e75203401d410cbd3f930
Cliford Pereira <cliford.pereira@gmail.com>
2025-09-25 19:11:53 +0530

    Bug-fixed: schema was trying to loading even when the user is not authenticated.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,14 +6,22 @@ const typedNav = nav as NavItem[]; // cast JSON
 
 const auth = useAuthStore();
 
-const { loadSchema } = useSchema();
-const data = await loadSchema();
-const resourceArray = extractTables(data ?? "");
+// keep menu reactive, initially null
+const menu = ref<NavItem | null>(null);
 
-const menu = buildMenu(resourceArray);
+if (auth.isAuthenticated) {
+  const { loadSchema } = useSchema();
+  const data = await loadSchema();
+  const resourceArray = extractTables(data ?? "");
+
+  menu.value = buildMenu(resourceArray);
+}
 
 const filteredNav = computed<NavItem[]>(() => {
-  return [...typedNav, menu].filter((item) => {
+  // merge typedNav with menu if available
+  const items = menu.value ? [...typedNav, menu.value] : [...typedNav];
+
+  return items.filter((item) => {
     if (item.authOnly) return auth.isAuthenticated;
     if (item.guestOnly) return !auth.isAuthenticated;
     return true;
